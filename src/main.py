e475104a279de5b161f182673b3f68cdd17a4d14
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-04-28 09:34:40 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+def main():
+  '''Introduce a while true loop that has the exit condition of ‘q’
+    Prompt the user to handle one of the following:
+      = Create a new account
+      = Update an existing account (using unique ID)
+      = Deposit/Withdrawal
+      = Check member status
+'''
+
+if __name__ == '__main__':
+  main()
