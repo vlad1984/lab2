1f49c5614e833efc59fbd966b099b5787da07750
vlad1984 <vladimirbeauge@sunyorange.edu>
2026-04-28 09:37:53 -0400

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,25 @@
+import unittest
+
+class TestBank(unittest.TestCase):
+    def run_unit_test():       
+        unittest.main()
+    
+    def test_calc_user_age(self):
+        pass
+        #TODO consider pass/fail cases
+
+    def test_get_creation_date(self):
+        pass
+        #TODO consider pass/fail cases
+        
+    def test_get_member_status(self):
+        pass
+        #TODO consider pass/fail cases
+    
+    def test_deposit(self):
+        pass
+        #TODO consider pass/fail cases
+        
+    def test_withdrawal(self):
+        pass
+        #TODO consider pass/fail cases
